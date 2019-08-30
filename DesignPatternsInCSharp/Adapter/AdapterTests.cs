cda79ee55c604437a791ed9739c918095ad50235
Steve Smith <steve@kentsmiths.com>
2019-08-30 12:41:24 -0400

    Read people from json file

--- DesignPatternsInCSharp/Adapter/AdapterTests.cs
+++ DesignPatternsInCSharp/Adapter/AdapterTests.cs
@@ -1,7 +1,4 @@
-﻿using Newtonsoft.Json;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Xunit;
 
 namespace DesignPatternsInCSharp.Adapter
@@ -16,30 +13,6 @@ namespace DesignPatternsInCSharp.Adapter
             var result = await service.ListCharacters();
 
             Assert.NotNull(result);
-        }
-    }
-
-    public class StarWarsCharacterDisplayService
-    {
-        public async Task<string> ListCharacters()
-        {
-            using (var client = new HttpClient())
-            {
-                string url = "https://swapi.co/api/people";
-                string result = await client.GetStringAsync(url);
-                var people =  JsonConvert.DeserializeObject<ApiResult<Person>>(result).Results;
-
-                var sb = new StringBuilder();
-                int nameWidth = 20;
-                sb.AppendLine($"{"NAME".PadRight(nameWidth)}   {"GENDER"}");
-                foreach (Person person in people)
-                {
-                    sb.AppendLine($"{person.Name.PadRight(nameWidth)}   {person.Gender}");
-                }
-
-                return sb.ToString();
-            }
-
         }
     }
 }
---
+++ DesignPatternsInCSharp/Adapter/StarWarsCharacterDisplayService.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Adapter
+{
+    public class StarWarsCharacterDisplayService
+    {
+        public async Task<string> ListCharacters()
+        {
+            List<Person> people;
+            //people = await GetPeopleFromSWAPI();
+            people = await GetPeopleFromLocalFile();
+            var sb = new StringBuilder();
+            int nameWidth = 20;
+            sb.AppendLine($"{"NAME".PadRight(nameWidth)}   {"GENDER"}");
+            foreach (Person person in people)
+            {
+                sb.AppendLine($"{person.Name.PadRight(nameWidth)}   {person.Gender}");
+            }
+
+            return sb.ToString();
+
+        }
+
+        private Task<List<Person>> GetPeopleFromLocalFile()
+        {
+            return Task.FromResult(JsonConvert.DeserializeObject<List<Person>>(File.ReadAllText(@"C:\dev\GitHub\DesignPatternsInCSharp\DesignPatternsInCSharp\Adapter\People.json")));
+        }
+
+        private static async Task<List<Person>> GetPeopleFromSWAPI()
+        {
+            var people = new List<Person>();
+
+            using (var client = new HttpClient())
+            {
+                string url = "https://swapi.co/api/people";
+                string result = await client.GetStringAsync(url);
+                people = JsonConvert.DeserializeObject<ApiResult<Person>>(result).Results;
+            }
+
+            return people;
+        }
+    }
+}
