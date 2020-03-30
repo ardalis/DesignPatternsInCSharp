3f957967e6b519480e40518aef4edd64d8ece7f6
Steve Smith <steve@kentsmiths.com>
2020-03-30 14:34:14 -0400

    Adding lazy loaded expensive entity

---
+++ DesignPatternsInCSharp/Proxy/VirtualProxy/ExpensiveEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatternsInCSharp.Proxy.VirtualProxy
+{
+    public class ExpensiveEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ DesignPatternsInCSharp/Proxy/VirtualProxy/ExpensiveToFullyLoad.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsInCSharp.Proxy.VirtualProxy
+{
+    public class VirtualExpensiveToFullyLoad : ExpensiveToFullyLoad
+    {
+        public override IEnumerable<ExpensiveEntity> AwayEntities 
+        {
+            get
+            {
+                if(base.AwayEntities == null)
+                {
+                    base.AwayEntities = new List<ExpensiveEntity>(ExpensiveDataSource.GetEntities(this));
+                }
+                return base.AwayEntities;
+            }
+            protected set => base.AwayEntities = value; 
+        }
+
+        public override IEnumerable<ExpensiveEntity> HomeEntities 
+        {
+            get
+            {
+                if (base.HomeEntities == null)
+                {
+                    base.HomeEntities = new List<ExpensiveEntity>(ExpensiveDataSource.GetEntities(this));
+                }
+                return base.HomeEntities;
+            }
+            protected set => base.HomeEntities = value;
+        }
+    }
+
+    public class ExpensiveToFullyLoad
+    {
+        public static ExpensiveToFullyLoad Create()
+        {
+            return new VirtualExpensiveToFullyLoad();
+        }
+
+        public virtual IEnumerable<ExpensiveEntity> HomeEntities { get; protected set; }
+        public virtual IEnumerable<ExpensiveEntity> AwayEntities { get; protected set; }
+        
+        public List<string> History { get; } = new List<string>();
+
+        public ExpensiveToFullyLoad()
+        {
+            History.Add("Constructor called.");
+        }
+    }
+}
