9fe18d64d755a4ed9b10e37d14b249af91eba911
Steve Smith <steve@kentsmiths.com>
2020-04-14 14:00:23 -0400

    Initial singleton

---
+++ DesignPatternsInCSharp/Singleton/v1/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatternsInCSharp.Singleton.v1
+{
+    public static class Logger
+    {
+        private static List<string> _log = new List<string>();
+
+        public static void Log(string message)
+        {
+            _log.Add(message);
+        }
+
+        public static void Clear()
+        {
+            _log.Clear();
+        }
+
+        public static string StringDump()
+        {
+            return string.Join(Environment.NewLine, _log);
+        }
+
+        public static IEnumerable<string> Output()
+        {
+            return _log.AsEnumerable();
+        }
+    }
+}
---
+++ DesignPatternsInCSharp/Singleton/v1/SingletonTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace DesignPatternsInCSharp.Singleton.v1
+{
+    public class SingletonInstance
+    {
+        [Fact]
+        public void ReturnsNonNullSingletonInstance()
+        {
+            var result = Singleton.Instance;
+
+            Assert.NotNull(result);
+            Assert.IsType<Singleton>(result);
+        }
+
+        [Fact]
+        public void OnlyCallsConstructorOnceGivenThreeInstanceCalls()
+        {
+            Logger.Clear();
+            var result1 = Singleton.Instance;
+            var result2 = Singleton.Instance;
+            var result3 = Singleton.Instance;
+
+            var log = Logger.Output();
+            Assert.Equal(1, log.Count(log => log.Contains("Constructor")));
+            Assert.Equal(3, log.Count(log => log.Contains("Instance")));
+
+        }
+    }
+}
