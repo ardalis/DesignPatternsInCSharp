f9bea6947cf77410ac39ba937c1688f4523a6ada
Steve Smith <steve@kentsmiths.com>
2020-04-16 11:01:32 -0400

    Singleton tests passing

--- DesignPatternsInCSharp/Singleton/v1/SingletonInstance.cs
+++ DesignPatternsInCSharp/Singleton/v1/SingletonInstance.cs
@@ -1,43 +1,42 @@
 ﻿using System.Linq;
+using System.Threading;
 using Xunit;
 using Xunit.Abstractions;
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 
 namespace DesignPatternsInCSharp.Singleton.v1
 {
-    [CollectionDefinition("one", DisableParallelization = true)]
     public class SingletonInstance
     {
         private readonly ITestOutputHelper _output;
         public SingletonInstance(ITestOutputHelper output)
         {
             _output = output;
+            SingletonTestHelpers.Reset();
+            Logger.Clear();
         }
 
         [Fact]
         public void ReturnsNonNullSingletonInstance()
         {
+            Assert.Null(SingletonTestHelpers.GetPrivateStaticInstance());
+
             var result = Singleton.Instance;
 
             Assert.NotNull(result);
             Assert.IsType<Singleton>(result);
-        }
-    }
 
-    [CollectionDefinition("two", DisableParallelization = true)]
-    public class SingletonInstance2
-    {
-        private readonly ITestOutputHelper _output;
-        public SingletonInstance2(ITestOutputHelper output)
-        {
-            _output = output;
+            Logger.Output().ToList().ForEach(h => _output.WriteLine(h));
         }
 
         [Fact]
         public void OnlyCallsConstructorOnceGivenThreeInstanceCalls()
         {
-            Logger.Clear();
+            Assert.Null(SingletonTestHelpers.GetPrivateStaticInstance());
             var result1 = Singleton.Instance;
+            Thread.Sleep(1);
             var result2 = Singleton.Instance;
+            Thread.Sleep(1);
             var result3 = Singleton.Instance;
 
             var log = Logger.Output();
