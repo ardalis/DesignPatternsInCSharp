c0f33102567d3ef4388a5c7e9c86f0f7614162c1
Steve Smith <steve@kentsmiths.com>
2020-04-16 13:23:17 -0400

    Adding v4 singleton and tests

---
+++ DesignPatternsInCSharp/Singleton/v4_LessLazy/Singleton.cs
@@ -0,0 +1,32 @@
+﻿namespace DesignPatternsInCSharp.Singleton.v4
+{
+    // Bad code
+    // Source: https://csharpindepth.com/articles/singleton
+    public sealed class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+
+        // reading this will initialize the _instance
+        public static readonly string GREETING = "Hi!";
+
+        // Tell C# compiler not to mark type as beforefieldinit (https://csharpindepth.com/articles/BeforeFieldInit)
+        static Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Logger.Log("Instance called.");
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+            // cannot be created except within this class
+            Logger.Log("Constructor invoked.");
+        }
+    }
+}
