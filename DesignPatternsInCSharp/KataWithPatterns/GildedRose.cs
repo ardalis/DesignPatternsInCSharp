32967ae1a186cf609fc33c58ae999649b5e217cd
Steve Smith <steve@kentsmiths.com>
2020-05-05 14:13:03 -0400

    Introduce ItemProxy

--- DesignPatternsInCSharp/KataWithPatterns/GildedRose.cs
+++ DesignPatternsInCSharp/KataWithPatterns/GildedRose.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace DesignPatternsInCSharp.KataWithPatterns
 {
@@ -17,79 +15,84 @@ namespace DesignPatternsInCSharp.KataWithPatterns
             this.Items = Items;
         }
 
-        public void UpdateQuality()
+        public void UpdateQuality(ItemProxy item)
         {
-            for (var i = 0; i < Items.Count; i++)
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Quality > 0)
                 {
-                    if (Items[i].Quality > 0)
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
+                        item.DecrementQuality();
                     }
                 }
-                else
+            }
+            else
+            {
+                if (item.Quality < 50)
                 {
-                    if (Items[i].Quality < 50)
+                    item.IncrementQuality();
+
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.SellIn < 11)
                         {
-                            if (Items[i].SellIn < 11)
+                            if (item.Quality < 50)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
+                                item.IncrementQuality();
                             }
+                        }
 
-                            if (Items[i].SellIn < 6)
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
+                                item.IncrementQuality();
                             }
                         }
                     }
                 }
+            }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.DecrementSellIn();
+            }
 
-                if (Items[i].SellIn < 0)
+            if (item.SellIn < 0)
+            {
+                if (item.Name != "Aged Brie")
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Quality > 0)
                         {
-                            if (Items[i].Quality > 0)
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
+                                item.DecrementQuality();
                             }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
                         }
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
+                        item.ResetQuality();
                     }
                 }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.IncrementQuality();
+                    }
+                }
+            }
+        }
+
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                UpdateQuality(new ItemProxy(Items[i]));
             }
         }
     }
