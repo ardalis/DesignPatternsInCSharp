807bc2561a1a0b5d9d633526fad995d84ef1cd51
Steve Smith <steve@kentsmiths.com>
2020-05-05 14:48:20 -0400

    Added BackstagePassesRule

---
+++ DesignPatternsInCSharp/KataWithPatterns/BackstagePassesRule.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatternsInCSharp.KataWithPatterns
+{
+    public class BackstagePassesRule : RuleBase
+    {
+        public override bool IsMatch(ItemProxy item)
+        {
+            return item.Name == "Backstage passes to a TAFKAL80ETC concert";
+        }
+
+        public override void UpdateItem(ItemProxy item)
+        {
+            item.IncrementQuality();
+            if (item.SellIn < 11)
+            {
+                item.IncrementQuality();
+            }
+
+            if (item.SellIn < 6)
+            {
+                item.IncrementQuality();
+            }
+            item.DecrementSellIn();
+            if (item.SellIn < 0)
+            {
+                item.ResetQuality();
+            }
+        }
+    }
+}
--- DesignPatternsInCSharp/KataWithPatterns/GildedRose.cs
+++ DesignPatternsInCSharp/KataWithPatterns/GildedRose.cs
@@ -15,37 +15,13 @@ namespace DesignPatternsInCSharp.KataWithPatterns
             this.Items = Items;
         }
 
-        public void UpdateBackstagePasses(ItemProxy item)
-        {
-            item.IncrementQuality();
-            if (item.SellIn < 11)
-            {
-                item.IncrementQuality();
-            }
-
-            if (item.SellIn < 6)
-            {
-                item.IncrementQuality();
-            }
-            item.DecrementSellIn();
-            if (item.SellIn < 0)
-            {
-                item.ResetQuality();
-            }
-        }
-
         public void UpdateQuality(ItemProxy item)
         {
-            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-            {
-                UpdateBackstagePasses(item);
-                return;
-            }
-
             var rules = new List<RuleBase>();
             rules.Add(new SulfurasRule());
             rules.Add(new ConjuredItemRule());
             rules.Add(new AgedBrieRule());
+            rules.Add(new BackstagePassesRule());
             rules.Add(new NormalItemRule());
             foreach (var rule in rules)
             {
