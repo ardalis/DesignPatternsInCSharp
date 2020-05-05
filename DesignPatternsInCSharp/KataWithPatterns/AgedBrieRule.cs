14dad12796857d1085b9a92cd95b05011e5dfb19
Steve Smith <steve@kentsmiths.com>
2020-05-05 14:46:14 -0400

    Added AgedBrieRule

---
+++ DesignPatternsInCSharp/KataWithPatterns/AgedBrieRule.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatternsInCSharp.KataWithPatterns
+{
+    public class AgedBrieRule : RuleBase
+    {
+        public override bool IsMatch(ItemProxy item)
+        {
+            return item.Name == "Aged Brie";
+        }
+
+        public override void UpdateItem(ItemProxy item)
+        {
+            item.IncrementQuality();
+            item.DecrementSellIn();
+            if (item.SellIn < 0)
+            {
+                item.IncrementQuality();
+            }
+        }
+    }
+}
--- DesignPatternsInCSharp/KataWithPatterns/GildedRose.cs
+++ DesignPatternsInCSharp/KataWithPatterns/GildedRose.cs
@@ -15,16 +15,6 @@ namespace DesignPatternsInCSharp.KataWithPatterns
             this.Items = Items;
         }
 
-        public void UpdateAgedBrie(ItemProxy item)
-        {
-            item.IncrementQuality();
-            item.DecrementSellIn();
-            if (item.SellIn < 0)
-            {
-                item.IncrementQuality();
-            }
-        }
-
         public void UpdateBackstagePasses(ItemProxy item)
         {
             item.IncrementQuality();
@@ -46,11 +36,6 @@ namespace DesignPatternsInCSharp.KataWithPatterns
 
         public void UpdateQuality(ItemProxy item)
         {
-            if (item.Name == "Aged Brie")
-            {
-                UpdateAgedBrie(item);
-                return;
-            }
             if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
                 UpdateBackstagePasses(item);
@@ -60,6 +45,7 @@ namespace DesignPatternsInCSharp.KataWithPatterns
             var rules = new List<RuleBase>();
             rules.Add(new SulfurasRule());
             rules.Add(new ConjuredItemRule());
+            rules.Add(new AgedBrieRule());
             rules.Add(new NormalItemRule());
             foreach (var rule in rules)
             {
