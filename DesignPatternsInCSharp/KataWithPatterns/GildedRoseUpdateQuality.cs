f21f32f679e5185528cd6bf98bdb6449bc2b072c
Steve Smith <steve@kentsmiths.com>
2020-05-05 11:28:01 -0400

    Starting GildedRose kata.

---
+++ DesignPatternsInCSharp/KataWithPatterns/GildedRoseUpdateQuality.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace DesignPatternsInCSharp.KataWithPatterns
+{
+    public class GildedRoseUpdateQuality
+    {
+        [Fact]
+        public void ReducesNormalItemQualityBy1()
+        {
+            int initialQuality = 10;
+            var normalItem = new Item { Name = "Normal Item", Quality = initialQuality, SellIn = 15 };
+            var service = new GildedRose(new List<Item> { normalItem });
+
+            service.UpdateQuality();
+
+            Assert.Equal(initialQuality - 1, normalItem.Quality);
+        }
+    }
+}
---
+++ DesignPatternsInCSharp/KataWithPatterns/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatternsInCSharp.KataWithPatterns
+{
+    /// <summary>
+    /// Source: https://github.com/emilybache/GildedRose-Refactoring-Kata/tree/master/csharpcore
+    /// Cannot touch this code (except to make properties virtual, if desired)
+    /// </summary>
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
