22ada08231cc1d8dc6f6572d8de596f684018ac3
Steve Smith <steve@kentsmiths.com>
2020-05-05 11:34:35 -0400

    Added tests for normal item logic

--- DesignPatternsInCSharp/KataWithPatterns/GildedRoseUpdateQuality.cs
+++ DesignPatternsInCSharp/KataWithPatterns/GildedRoseUpdateQuality.cs
@@ -5,16 +5,63 @@ namespace DesignPatternsInCSharp.KataWithPatterns
 {
     public class GildedRoseUpdateQuality
     {
+        private List<Item> _items = new List<Item>();
+        private GildedRose _service;
+        private const int INITIAL_QUALITY = 10;
+        private const int INITIAL_SELL_IN = 20;
+
+        public GildedRoseUpdateQuality()
+        {
+            _service = new GildedRose(_items);
+        }
+
+        private Item GetNormalItem()
+        {
+            return new Item { Name = "Normal Item", Quality = INITIAL_QUALITY, SellIn = INITIAL_SELL_IN };
+        }
+
         [Fact]
         public void ReducesNormalItemQualityBy1()
         {
-            int initialQuality = 10;
-            var normalItem = new Item { Name = "Normal Item", Quality = initialQuality, SellIn = 15 };
-            var service = new GildedRose(new List<Item> { normalItem });
+            var normalItem = GetNormalItem();
+            _items.Add(normalItem);
+            _service.UpdateQuality();
 
-            service.UpdateQuality();
-
-            Assert.Equal(initialQuality - 1, normalItem.Quality);
+            Assert.Equal(INITIAL_QUALITY - 1, normalItem.Quality);
         }
+
+        [Fact]
+        public void DoesNotReduceQualityBelowZero()
+        {
+            var normalItem = GetNormalItem();
+            normalItem.Quality = 0;
+            _items.Add(normalItem);
+            _service.UpdateQuality();
+
+            Assert.Equal(0, normalItem.Quality);
+        }
+
+        [Fact]
+        public void ReducesNormalItemSellInBy1()
+        {
+            var normalItem = GetNormalItem();
+            _items.Add(normalItem);
+            _service.UpdateQuality();
+
+            Assert.Equal(INITIAL_SELL_IN - 1, normalItem.SellIn);
+        }
+
+        [Fact]
+        public void DoesReduceSellInBelowZero()
+        {
+            var normalItem = GetNormalItem();
+            normalItem.SellIn = 0;
+            _items.Add(normalItem);
+            _service.UpdateQuality();
+
+            Assert.Equal(-1, normalItem.SellIn);
+        }
+
+
     }
 }
