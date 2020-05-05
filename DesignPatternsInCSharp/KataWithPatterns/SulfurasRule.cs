65509b71e4ca31be1a036f60a935627d8468bc8c
Steve Smith <steve@kentsmiths.com>
2020-05-05 15:10:37 -0400

    Fixed test by adjusting SulfurasRule

--- DesignPatternsInCSharp/KataWithPatterns/SulfurasRule.cs
+++ DesignPatternsInCSharp/KataWithPatterns/SulfurasRule.cs
@@ -12,6 +12,11 @@
             // do nothing
         }
 
+        public override void AdjustSellIn(ItemProxy item)
+        {
+            // do nothing
+        }
+
         public override void AdjustQualityForNegativeSellIn(ItemProxy item)
         {
             // do nothing
