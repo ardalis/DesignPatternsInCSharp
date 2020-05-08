42265592d84f1f493dc69a9ec835790f56e5ccc1
Steve Smith <steve@kentsmiths.com>
2020-05-08 13:20:22 -0400

    Adding better tests for pizza baking service

--- DesignPatternsInCSharp/TemplateMethod/Pizza.cs
+++ DesignPatternsInCSharp/TemplateMethod/Pizza.cs
@@ -1,4 +1,12 @@
-﻿namespace DesignPatternsInCSharp.TemplateMethod
+﻿using System.Collections.Generic;
+
+namespace DesignPatternsInCSharp.TemplateMethod
 {
-    public class Pizza : PanFood { }
+    public class Pizza : PanFood 
+    {
+        public string CrustType { get; set; } = "no crust";
+        public int NumSlices { get; set; } = 1;
+        public List<string> Toppings { get; private set; } = new List<string>();
+        public bool WasBaked { get; set; }
+    }
 }
--- DesignPatternsInCSharp/TemplateMethod/PizzaBakingService.cs
+++ DesignPatternsInCSharp/TemplateMethod/PizzaBakingService.cs
@@ -5,8 +5,6 @@ namespace DesignPatternsInCSharp.TemplateMethod
 
     public class PizzaBakingService : PanFoodServiceBase<Pizza>
     {
-        private Pizza _pizza;
-
         public PizzaBakingService(LoggerAdapter logger) : base(logger)
         {
         }
@@ -14,22 +12,26 @@ namespace DesignPatternsInCSharp.TemplateMethod
         protected override void PrepareCrust()
         {
             _logger.Log("Rolling out and hand tossing the dough");
+            _item.CrustType = "Thin";
         }
 
         protected override void AddToppings()
         {
             _logger.Log("Adding pizza toppings");
+            _item.Toppings.Add("Pepperoni");
+            _item.Toppings.Add("Sausage");
         }
 
         protected override void Bake()
         {
             _logger.Log("Baking for 15 minutes");
+            _item.WasBaked = true;
         }
 
         protected override void Slice()
         {
             _logger.Log("Cutting into 8 slices.");
+            _item.NumSlices = 8;
         }
-
     }
 }
