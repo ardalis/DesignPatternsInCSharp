991fefa205bfe086a39d83f9256a2c642b418df1
Steve Smith <steve@kentsmiths.com>
2020-05-19 23:20:42 -0400

    Adding inheritance example

---
+++ DesignPatternsInCSharp/TemplateMethod/Inheritance/Base.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPatternsInCSharp.TemplateMethod.Inheritance
+{
+    public abstract class Base
+    {
+        private bool _importantSetting;
+        public virtual void Do()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _importantSetting = true;
+        }
+    }
+
+    public class Child : Base
+    {
+        public override void Do()
+        {
+            // do other stuff
+        }
+    }
+}
