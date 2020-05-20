662f0875d56a0f70b1bca1dfe45bb8e3b00e3ad8
Steve Smith <steve@kentsmiths.com>
2020-05-19 23:23:04 -0400

    Added TemplateBase

---
+++ DesignPatternsInCSharp/TemplateMethod/Inheritance/TemplateBase.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignPatternsInCSharp.TemplateMethod.Inheritance
+{
+    public abstract class TemplateBase
+    {
+        private bool _importantSetting;
+        public void Do()
+        {
+            BeforeDoing();
+            Initialize();
+            AfterDone();
+        }
+
+        public virtual void BeforeDoing()
+        { }
+
+        public abstract void AfterDone();
+
+        private void Initialize()
+        {
+            _importantSetting = true;
+        }
+    }
+
+    public class TemplateChild : TemplateBase
+    {
+        public override void AfterDone()
+        {
+            // do other stuff
+        }
+    }
+}
