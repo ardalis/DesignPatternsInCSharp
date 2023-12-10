d07b1d62cb65819006a0878f5ae5ff5e9fe0cc10
Catherinekaram11 <53629114+Catherinekaram11@users.noreply.github.com>
2023-12-10 12:29:36 +0200

    Singleton_AdamAndEve_KATA

---
+++ DesignPatternsInCSharp/KATA/Singleton_AdamAndEve/Human.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsInCSharp.KATA.Singleton_AdamAndEve
+{
+    public interface Human
+    {
+        string Name { get; set; }
+        Female Mother { get; set; }
+        Male Father { get; set; }
+        public bool ValidateAdamOrEve(Human human)
+        {
+            if (human.Father == null || human.Mother == null)
+            {
+                if (human.Name != "Adam" && human.Name != "Eve")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
